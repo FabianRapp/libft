61543efc6255a727bf5c23b66955a72d7b33adde
FabianRapp <fa.rapp@web.de>
2024-10-08 02:08:43 +0200

    CHANGE(strncmp): now return a val if one ptr is not NULL and the otehr is NULL instead of segfaulting

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,6 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	const unsigned char	*str1;
 	const unsigned char	*str2;
 
+	if (!s1 && s2)
+		return (*s2);
+	if (!s2 && s1)
+		return (*s1);
 	str1 = (const unsigned char *)s1;
 	str2 = (const unsigned char *)s2;
 	while (n && *str1 && *str2 && !(*str1 - *str2))
