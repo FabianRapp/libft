3323de247b5b42a3cfccdb0622b5bf9c464dc688
FabianRapp <fa.rapp@web.de>
2024-10-09 22:25:50 +0200

    NORM(fixed norm)

--- ft_assert.c
+++ ft_assert.c
@@ -1,10 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_assert.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frapp <frapp@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 22:18:18 by frapp             #+#    #+#             */
+/*   Updated: 2024/10/09 22:25:05 by frapp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 //how to call to crash the programm if a is not 10:
 //ft_assert(a == 10, __FILE__, __LINE__, "a is not 10);
-#if defined(__x86_64__) || defined(__i386__)
-
+//#if defined(__x86_64__) || defined(__i386__)
 void	ft_assert(bool cond, char *file, int line, char *msg)
 {
 	if (cond)
@@ -12,22 +22,23 @@ void	ft_assert(bool cond, char *file, int line, char *msg)
 	ft_fprintf(2, "Assert in %s line %d: %s\n:", file, line, msg);
 	__asm__("INT3\n\t");
 }
-#elif defined(__arm__)
-void	ft_assert(bool cond, char *file, int line, char *msg)
-{
-	if (cond)
-		return ;
-	ft_fprintf(2, "Assert in %s line %d: %s\n:", file, line, msg);
-	__asm__("BKPT #0");
-}
+// #elif defined(__arm__)
 
-#else
-void	ft_assert(bool cond, char *file, int line, char *msg)
-{
-	if (cond)
-		return ;
-	ft_fprintf(2, "Assert in %s line %d: %s\n:", file, line, msg);
-	exit(1);
-}
+// void	ft_assert(bool cond, char *file, int line, char *msg)
+// {
+// 	if (cond)
+// 		return ;
+// 	ft_fprintf(2, "Assert in %s line %d: %s\n:", file, line, msg);
+// 	__asm__("BKPT #0");
+// }
+// #else
 
-#endif //ARCHITECTURE
+// void	ft_assert(bool cond, char *file, int line, char *msg)
+// {
+// 	if (cond)
+// 		return ;
+// 	ft_fprintf(2, "Assert in %s line %d: %s\n:", file, line, msg);
+// 	exit(1);
+// }
+
+// #endif //ARCHITECTURE
