2dadca150f5525f437defa42412e3d0fb6d50a8a
FabianRapp <fa.rapp@web.de>
2024-10-10 20:08:28 +0200

    FEAT(added macro assert)

---
+++ libft_macros.h
@@ -0,0 +1,31 @@
+#ifndef LIBFT_MACROS
+# define LIBFT_MACROS
+
+
+# ifdef NDEBUG
+
+#  ifndef FT_ASSERT
+#   define FT_ASSERT(cond) \
+	do { \
+		if(!(cond)) {\
+			__builtin_unreachable(); \
+		}\
+	} while(0)
+#  endif //FT_ASSERT
+
+#  else //NDEBUG
+#   include <stdio.h>
+#   include <assert.h>
+
+#  ifndef FT_ASSERT
+#   define FT_ASSERT(cond) \
+	do { \
+		if (!(cond)) {\
+			assert(cond); \
+		} \
+	} while(0)
+#  endif //FT_ASSERT
+
+# endif //NDEBUG
+
+#endif //LIBFT_MACROS
