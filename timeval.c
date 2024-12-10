d45cafffab05f6f0d1dff25e91837bb617caaa61
FabianRapp <fa.rapp@web.de>
2024-12-10 15:17:45 +0100

    timeval.c

--- timeval.c
+++ timeval.c
@@ -1,1 +1,21 @@
 #include "libft.h"
+
+struct timeval	add_timeval(struct timeval a, struct timeval b) {
+	a.tv_sec += b.tv_sec;
+	a.tv_usec += b.tv_usec;
+	while (a.tv_usec >= 1000000) {
+		a.tv_sec++;
+		a.tv_usec -= 1000000;
+	}
+	return (a);
+}
+
+struct timeval	sub_timeval(struct timeval a, struct timeval b) {
+	a.tv_sec -= b.tv_sec;
+	a.tv_usec -= b.tv_usec;
+	while (a.tv_usec < 0) {
+		a.tv_sec--;
+		a.tv_usec += 1000000;
+	}
+	return (a);
+}
